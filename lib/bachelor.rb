9b19fdad16128dc12fcf63ea492779c51aa6d03f
Adam Giegel <adam.giegel@gmail.com>
2018-10-25 16:55:15 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,56 +1,49 @@
 def get_first_name_of_season_winner(data, season)
-   data.each do |key, value|
-     if key == season
-      # if key(season) = the season being passed in
-       value.each do |contenstant|
-        # then iterate over the values...for each value 
-         if contenstant.has_value?("Winner")
-          # if the value(which is the key now) has a value of "winner"
-           return contenstant["name"].split(" ")[0]
-          # return the first name
-         end
-       end
-     end
-   end
- end		 
- 
+  data[season].each do |contestant_hash|
+    if contestant_hash["status"].downcase == "winner"
+      return contestant_hash["name"].split(" ").first
+    end
+  end
+end
+
 def get_contestant_name(data, occupation)
-  data.each do |key, value|
-    value.each do |info|
-    if info["occupation"] == occupation
-    return info["name"]
+  data.each do |season, contestants|
+    contestants.each do |contestant_hash|
+      if contestant_hash["occupation"] == occupation
+        return contestant_hash["name"]
       end
     end
   end
-end	 
+end
 
 def count_contestants_by_hometown(data, hometown)
   count = 0
-   data.each do |season, info|
-     info.each do |contestant|
-       if contestant["hometown"] == hometown
-         count += 1
-       end
-     end
-   end
-   count
- end		 
- 
+  data.each do |season, contestants|
+    contestants.each do |contestant_hash|
+      if contestant_hash["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  count
+end
+
 def get_occupation(data, hometown)
-  data.each do |season, info|
-     info.each do |contestant|
-       if contestant["hometown"] == hometown
-         return contestant["occupation"]
-       end
-     end
-   end
- end		 
+  data.each do |season, contestants|
+    contestants.each do |contestant_hash|
+      if contestant_hash["hometown"] == hometown
+        return contestant_hash['occupation']
+      end
+    end
+  end
+end
 
 def get_average_age_for_season(data, season)
-  sum = 0
-  data[season].each do |contestant|
-    sum += contestant["age"].to_i
+  age_total = 0
+  num_of_contestants = 0
+  data[season].each do |contestant_hash|
+    age_total += (contestant_hash["age"]).to_i
+    num_of_contestants += 1
   end
-   average = sum.to_f/data[season].length
-   average.round
-end		 
+  (age_total / num_of_contestants.to_f).round(0)
+end
