65e5a5f886aa15d33871c367b8b52f245de8c408
Adam Giegel <adam.giegel@gmail.com>
2018-10-25 16:53:44 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,49 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
-end
-
+   data.each do |key, value|
+     if key == season
+      # if key(season) = the season being passed in
+       value.each do |contenstant|
+        # then iterate over the values...for each value 
+         if contenstant.has_value?("Winner")
+          # if the value(which is the key now) has a value of "winner"
+           return contenstant["name"].split(" ")[0]
+          # return the first name
+         end
+       end
+     end
+   end
+ end		 
+ 
 def get_contestant_name(data, occupation)
-  # code here
-end
+  data.each do |key, value|
+    value.each do |info|
+    if info["occupation"] == occupation
+    return info["name"]
+      end
+    end
+  end
+end	 
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
-end
-
+  count = 0
+   data.each do |season, info|
+     info.each do |contestant|
+       if contestant["hometown"] == hometown
+         count += 1
+       end
+     end
+   end
+   count
+ end		 
+ 
 def get_occupation(data, hometown)
-  # code here
-end
+  data.each do |season, info|
+     info.each do |contestant|
+       if contestant["hometown"] == hometown
+         return contestant["occupation"]
+       end
+     end
+   end
+ end		 
 
-def get_average_age_for_season(data, season)
-  # code here
-end
+ 
